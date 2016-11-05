ec50be43bf36109f19769d62658042b26f11a31e
Andrea Leopardi <an.leopardi@gmail.com>
2016-11-05 21:02:41 +0100

    Use parentheses in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,32 +4,32 @@ defmodule ShortMaps.Mixfile do
   @version "0.1.1"
   @repo_url "https://github.com/whatyouhide/short_maps"
 
-  def project do
+  def project() do
     [app: :short_maps,
      version: @version,
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
+     deps: deps(),
      # Hex
-     package: hex_package,
+     package: hex_package(),
      description: "Implementation of a ~m sigil for ES6-like maps in Elixir",
      # Docs
      name: "ShortMaps",
      docs: [source_ref: "v#{@version}", main: "ShortMaps", source_url: @repo_url]]
   end
 
-  def application do
+  def application() do
     [applications: []]
   end
 
-  defp hex_package do
+  defp hex_package() do
     [maintainers: ["Andrea Leopardi"],
      licenses: ["MIT"],
      links: %{"GitHub" => @repo_url}]
   end
 
-  defp deps do
+  defp deps() do
     [{:earmark, ">= 0.0.0", only: :docs},
      {:ex_doc, ">= 0.0.0", only: :docs}]
   end
